4f49de940fa7c750da86cb9072bd43958060c35f
Dev <flashytesting@gmail.com>
2023-06-23 16:15:27 +0530

    Make responsive NavBar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,15 +2,15 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 const Nav = () => {
     const pathname = usePathname();
-    console.log(pathname);
 
     return (
-        <div className='absolute top-0 left-0 right-0 p-3 flex justify-between items-center'>
-            <h1 className='font-bold uppercase'>Dev Kaul</h1>
-            <ul className='flex items-center gap-4 text-sm'>
+        <div className='absolute top-0 left-0 right-0 p-3 mt-1 flex justify-between items-center'>
+            <Link className='font-bold uppercase' href='/'>Dev Kaul</Link> <RxHamburgerMenu className='text-2xl cursor-pointer sm:hidden' onClick={() => console.log('click')} />
+            <ul className='items-center text-sm hidden sm:flex gap-5'>
                 <Link className={`${pathname === '/' ? 'text-white' : 'text-gray-500'} cursor-pointer hover:text-white`} href='/'>Home</Link>
                 <Link className={`${pathname === '/blog' ? 'text-white' : 'text-gray-500'} cursor-pointer hover:text-white`} href='/blog'>Blog</Link>
                 <Link className={`${pathname === '/project' ? 'text-white' : 'text-gray-500'} cursor-pointer hover:text-white`} href='/project'>Project</Link>
