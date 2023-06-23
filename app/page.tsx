1cfb3b929caabfdf2607cbcd859fde75ac610241
Dev <flashytesting@gmail.com>
2023-06-23 17:21:33 +0530

    Update color change transition

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ const Home = () => {
             <div className='sm:w-3/4'>
                 <p className='leading-7'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nunc mi, imperdiet in lacinia sed, mollis sed ipsum. Phasellus vehicula augue nec dapibus interdum. Ut diam metus, pharetra id vehicula id, consectetur vel velit. Aliquam a dolor ut tortor consectetur luctus sit amet non nulla. Morbi volutpat ornare ante ut ultrices. Nunc id dolor sollicitudin, placerat lectus et, dictum sapien. Sed lacus dolor, pulvinar vitae justo auctor, blandit gravida lacus.</p>
                 <div className='mt-7 text-xs flex flex-col justify-around gap-3 sm:flex-row'>
-                    <a className='border border-white w-full h-10 rounded-md flex justify-center items-center cursor-pointer font-semibold hover:bg-white hover:text-black' href='https://github.com/BugReportOnWeb' target='_blank'>GitHub</a>
-                    <a className='border border-white w-full h-10 rounded-md flex justify-center items-center cursor-pointer font-semibold hover:bg-white hover:text-black' href='https://twitter.com/devkaul0' target='_blank'>Twitter</a>
+                    <a className='border border-white w-full h-10 rounded-md flex justify-center items-center cursor-pointer font-semibold transition-colors ease-in-out hover:bg-white hover:text-black' href='https://github.com/BugReportOnWeb' target='_blank'>GitHub</a>
+                    <a className='border border-white w-full h-10 rounded-md flex justify-center items-center cursor-pointer font-semibold transition-colors ease-in-out hover:bg-white hover:text-black' href='https://twitter.com/devkaul0' target='_blank'>Twitter</a>
                 </div>
             </div>
         </div>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,12 +9,13 @@ const Nav = () => {
 
     return (
         <div className='absolute top-0 left-0 right-0 p-3 mt-1 flex justify-between items-center'>
-            <Link className='font-bold uppercase' href='/'>Dev Kaul</Link> <RxHamburgerMenu className='text-2xl cursor-pointer sm:hidden' onClick={() => console.log('click')} />
-            <ul className='items-center text-sm hidden sm:flex gap-5'>
-                <Link className={`${pathname === '/' ? 'text-white' : 'text-gray-500'} cursor-pointer hover:text-white`} href='/'>Home</Link>
-                <Link className={`${pathname === '/blog' ? 'text-white' : 'text-gray-500'} cursor-pointer hover:text-white`} href='/blog'>Blog</Link>
-                <Link className={`${pathname === '/project' ? 'text-white' : 'text-gray-500'} cursor-pointer hover:text-white`} href='/project'>Project</Link>
-                <Link className={`${pathname === '/contact' ? 'text-white' : 'text-gray-500'} cursor-pointer hover:text-white`} href='/contact'>Contact</Link>
+            <Link className='font-bold uppercase' href='/'>Dev Kaul</Link>
+            <RxHamburgerMenu className='text-2xl cursor-pointer sm:hidden' onClick={() => console.log('click')} />
+            <ul className='items-center text-sm hidden sm:flex gap-5 '>
+                <Link className={`${pathname === '/' ? 'text-white' : 'text-gray-500'} cursor-pointer transition-colors ease-in-out hover:text-white`} href='/'>Home</Link>
+                <Link className={`${pathname === '/blog' ? 'text-white' : 'text-gray-500'} cursor-pointer transition-colors ease-in-out hover:text-white`} href='/blog'>Blog</Link>
+                <Link className={`${pathname === '/project' ? 'text-white' : 'text-gray-500'} cursor-pointer transition-colors ease-in-out hover:text-white`} href='/project'>Project</Link>
+                <Link className={`${pathname === '/contact' ? 'text-white' : 'text-gray-500'} cursor-pointer transition-colors ease-in-out hover:text-white`} href='/contact'>Contact</Link>
             </ul>
         </div>
     )
