f4af7a1471d29ab201f508536e50c40ab13b4054
Dev <flashytesting@gmail.com>
2023-07-11 13:41:30 +0530

    Change layout for blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 const Blog = () => {
     return (
-        <h1 className='p-5 font-bold text-2xl border border-blue-500'>Blog Page</h1>
+        <h1 className='font-extralight italic mt-10 text-center underline decoration-slate-500 underline-offset-8 sm:mt-0'>Under Construction :&#39;)</h1>
     )
 }
 
