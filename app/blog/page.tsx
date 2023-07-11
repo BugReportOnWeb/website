ccb92c16d0d6d013ae19d58ba2168e7f9cc4cc4d
Dev <flashytesting@gmail.com>
2023-07-11 20:26:08 +0530

    Minor update on UI

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 const Blog = () => {
     return (
-        <h1 className='font-extralight italic mt-10 text-center underline decoration-slate-500 underline-offset-8 sm:mt-0'>Under Construction :&#39;)</h1>
+        <h1 className='font-extrabold text-center text-2xl sm:text-6xl'>Coming soon...</h1>
     )
 }
 
