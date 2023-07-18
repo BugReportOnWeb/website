673bf80712c6233a3fccd3beac64ca010ad428cf
Dev <flashytesting@gmail.com>
2023-07-18 14:54:30 +0530

    Increase max width of main section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
 const Home = () => {
     return (
         <div className='h-full pt-[6.75rem] sm:flex sm:justify-center sm:items-center sm:pt-5'>
-            <div className='max-w-sm mx-auto flex flex-col px-5 pb-7 justify-center items-center gap-8 sm:flex-row-reverse sm:items-start sm:pb-0 sm:max-w-[40rem] sm:gap-5'>
+            <div className='max-w-sm mx-auto flex flex-col px-5 pb-7 justify-center items-center gap-8 sm:flex-row-reverse sm:items-start sm:pb-0 sm:max-w-2xl sm:gap-5'>
                 <Image height={170} width={170} className='rounded-full' src='/profile.png' alt="Author's Photo" priority />
                 <div className='sm:w-4/5'>
                     <p className='leading-8'>Hey! I&apos;m Dev, a hobbyist developer from Shimla, HP and welcome to my space. I code for fun, creating silly scripts and websites, and diving into all things tech before 2092. Oh, and music? It&apos;s my true passion and brings me so much joy. Feel free to wander around and checkout my work and stuff. Take your time, no pressure! :)</p>
