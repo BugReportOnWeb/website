158159e8f681f2138738f52f59547e776fc7ac7f
Dev <flashytesting@gmail.com>
2023-10-23 02:54:00 +0530

    Update email text and receiver field

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,24 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const API = process.env.RESEND_API_KEY;
-const resend = new Resend(API);
+const RECEIVER_EMAIL = process.env.NEXT_PUBLIC_EMAIL;
+const API_KEY = process.env.RESEND_API_KEY;
+
+const resend = new Resend(API_KEY);
 
 export const POST = async (req: NextRequest) => {
     const { name, email, message } = await req.json();
-
     const newMessage = `${name} wrote - ${message}`;
 
     try {
         // TODO: Make a react template for the message
         const res = await resend.emails.send({
             from: 'Website contact form <onboarding@resend.dev>',
-            to: 'devasheesh0909@gmail.com',
+            to: RECEIVER_EMAIL as string,
             reply_to: email,
             subject: 'New message from website contact form',
-            html: newMessage,
-
+            text: newMessage,
         })
+
         return NextResponse.json({ res });
     } catch (error) {
         return NextResponse.json({ error });
