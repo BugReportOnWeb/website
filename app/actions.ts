2ba01b5165bb675543ff8fe4c7bc6186020bb158
Dev <flashytesting@gmail.com>
2023-10-28 23:42:46 +0530

    Add server action for sending mail

---
+++ app/actions.ts
@@ -0,0 +1,34 @@
+"use server"
+
+import { Resend } from "resend";
+
+const RECEIVER_EMAIL = process.env.NEXT_PUBLIC_EMAIL;
+const API_KEY = process.env.RESEND_API_KEY;
+
+const resend = new Resend(API_KEY);
+
+const sendMail = async (formData: FormData) => {
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const message = formData.get('message');
+
+    const newMessage = `'${name}' wrote - ${message}`;
+
+    try {
+        // TODO: Make a react template for the message
+        // CHECK: Correct way to typecase 'as string'?
+        const res = await resend.emails.send({
+            from: 'Website contact form <onboarding@resend.dev>',
+            to: RECEIVER_EMAIL as string,
+            reply_to: email as string,
+            subject: 'New message from website contact form',
+            text: newMessage,
+        })
+
+        return { res };
+    } catch (error) {
+        return { error };
+    }
+}
+
+export { sendMail };
