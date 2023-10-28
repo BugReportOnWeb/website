8df00afb4768ea7188018ed5f9c2feef1c531cd7
Dev <flashytesting@gmail.com>
2023-10-28 23:44:30 +0530

    Update send mail action function on contact page

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,9 +1,9 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { BiSolidRightArrow } from 'react-icons/bi';
-import { FormData } from '@/types/FormData';
+import { sendMail } from '@/app/actions';
+import { ContactFormData } from '@/types/ContactFormData';
 
 const EMAIL = process.env.NEXT_PUBLIC_EMAIL;
-const env = process.env.NODE_ENV;
 
 // CHECK: Storage for constant values?
 const defaultFormData = {
@@ -13,52 +13,36 @@ const defaultFormData = {
 }
 
 const ContactForm = () => {
-    const [formData, setFormData] = useState<FormData>(defaultFormData);
-    const [loading, setLoading] = useState(false);
+    const [contactFormData, setContactFormData] = useState<ContactFormData>(defaultFormData);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setFormData(prevData => ({
+        setContactFormData(prevData => ({
             ...prevData,
             [e.target.name]: e.target.value
         }));
     }
 
-    const sendMail = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setLoading(true);
+    const handleMailAction = async (formData: FormData) => {
+        // TODO: Loading bug
+        setIsLoading(true);
 
-        // CHECK: Right way to do 'dev' vs 'prod'?
-        try {
-            const res = await fetch(env == 'development'
-                ? `http://localhost:3000/api/send`
-                : `https://devkaul.vercel.app/api/send`, {
-                method: 'POST',
-                headers: { "Content-Type": "application/json", },
-                body: JSON.stringify(formData)
-            })
+        // TODO: Clean up this area
+        const res = await sendMail(formData);
+        console.log(res);
 
-            const data = await res.json();
-
-            // TODO/CHECK: Do something useful with these consoles (if necessary)
-            console.log(data);
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
-
-        // TODO: Add loading state for resend api
-        setFormData(defaultFormData);
+        setContactFormData(defaultFormData);
+        setIsLoading(false);
     }
 
     return (
-        <form onSubmit={sendMail} className='w-full flex flex-col justify-center gap-5'>
+        <form action={handleMailAction} className='w-full flex flex-col justify-center gap-5'>
             <a href={`mailto:${EMAIL}`} className='w-fit text-start flex items-center'><BiSolidRightArrow className='mr-2' />{EMAIL}</a>
             <input
                 type='text'
                 name='name'
                 onChange={handleChange}
-                value={formData.name}
+                value={contactFormData.name}
                 required
                 maxLength={500}
                 placeholder='Name'
@@ -68,7 +52,7 @@ const ContactForm = () => {
                 type='email'
                 name='email'
                 onChange={handleChange}
-                value={formData.email}
+                value={contactFormData.email}
                 required
                 maxLength={500}
                 placeholder='Email Address'
@@ -77,13 +61,17 @@ const ContactForm = () => {
             <textarea
                 name='message'
                 onChange={handleChange}
-                value={formData.message}
+                value={contactFormData.message}
                 required
                 maxLength={5000}
                 placeholder='Type your message here.'
                 className='rounded-md bg-inherit border border-[#242F2B] py-2 px-3 text-sm w-full h-40 outline-white outline-offset-2 focus:outline focus:outline-2'
             />
-            <button type='submit' disabled={loading} className='mx-auto bg-[#f8fafc] font-medium text-[#020205] w-full h-10 rounded-md flex justify-center items-center cursor-pointer transition-colors ease-in-out hover:bg-[#f8fafc]/90 disabled:cursor-wait'>Send Email</button>
+            <button 
+                type='submit'
+                disabled={isLoading}
+                className='mx-auto bg-[#f8fafc] font-medium text-[#020205] w-full h-10 rounded-md flex justify-center items-center cursor-pointer transition-colors ease-in-out hover:bg-[#f8fafc]/90 disabled:cursor-wait'
+            >Send Email</button>
         </form>
     )
 }
