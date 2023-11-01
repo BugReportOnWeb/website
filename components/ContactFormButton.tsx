32fbb9cc303397862a92991f9296fe6488d5c5b7
Dev <flashytesting@gmail.com>
2023-11-02 02:10:22 +0530

    Create separate component for contact form button

---
+++ components/ContactFormButton.tsx
@@ -0,0 +1,15 @@
+import { useFormStatus } from "react-dom";
+
+const ContactFormButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+        <button
+            type='submit'
+            disabled={pending}
+            className='mx-auto bg-[#f8fafc] font-medium text-[#020205] w-full h-10 rounded-md flex justify-center items-center cursor-pointer transition-colors ease-in-out hover:bg-[#f8fafc]/90 disabled:cursor-wait'
+        >{pending ? 'Sending...' : 'Send Email'}</button>
+    )
+}
+
+export default ContactFormButton;
