92f803cb07ba64e0d048c60bb8262602dcf7daea
Dev <flashytesting@gmail.com>
2023-11-04 20:09:11 +0530

    Fix separate blog linking issue

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,17 +5,17 @@ import Link from "next/link";
 const Blog = () => {
     return (
         <div className='h-full flex justify-center'>
-            <Link href='/' className='max-w-4xl absolute mt-20 px-5 pt-5 pb-10 flex flex-col justify-center items-center gap-[1.85rem] sm:gap-11 sm:pt-10'>
+            <div className='max-w-4xl absolute mt-20 px-5 pt-5 pb-10 flex flex-col justify-center items-center gap-[1.85rem] sm:gap-11 sm:pt-10'>
                 {blogs.map((blog, index) => (
-                    <div key={index} className='p-6 border border-[#27272A] rounded-lg flex flex-col justify-center items-start gap-3 sm:p-10 hover:bg-[#040C14]'>
+                    <Link href='/' key={index} className='p-6 border border-[#27272A] rounded-lg flex flex-col justify-center items-start gap-3 sm:p-10 hover:bg-[#040C14]'>
                         <div>
                             <h1 className='font-bold text-2xl text-[#ededed]'>{blog.title}</h1>
                             <h1 className='text-xs mt-[0.425rem] text-[#ededed]/40'>{blog.date}</h1>
                         </div>
                         <p className='text-sm leading-[26px] text-[#ededed]/80'>{blog.description}</p>
-                    </div>
+                    </Link>
                 ))}
-            </Link>
+            </div>
             {/* <Footer component='blog' /> */}
         </div>
     )
