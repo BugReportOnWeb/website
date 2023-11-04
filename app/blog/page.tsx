0b9fd504f7436bb56c680e5978c4337041d0b79f
Dev <flashytesting@gmail.com>
2023-11-04 20:17:47 +0530

    Change hover color of blog card

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,12 +2,18 @@
 import blogs from "@/blogs/data";
 import Link from "next/link";
 
+// Possible other colors (hover: lighter -> darker):
+// #06101A
+// #050F19 (current)
+// #050D17
+// #040C14
+
 const Blog = () => {
     return (
         <div className='h-full flex justify-center'>
             <div className='max-w-4xl absolute mt-20 px-5 pt-5 pb-10 flex flex-col justify-center items-center gap-[1.85rem] sm:gap-11 sm:pt-10'>
                 {blogs.map((blog, index) => (
-                    <Link href='/' key={index} className='p-6 border border-[#27272A] rounded-lg flex flex-col justify-center items-start gap-3 sm:p-10 hover:bg-[#040C14]'>
+                    <Link href='/' key={index} className='p-6 border border-[#27272A] rounded-lg flex flex-col justify-center items-start gap-3 sm:p-10 hover:bg-[#050F19]'>
                         <div>
                             <h1 className='font-bold text-2xl text-[#ededed]'>{blog.title}</h1>
                             <h1 className='text-xs mt-[0.425rem] text-[#ededed]/40'>{blog.date}</h1>
