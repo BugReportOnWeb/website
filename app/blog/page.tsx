8cc411380dfe96ac1c16071fea780efb492645ec
Dev <flashytesting@gmail.com>
2023-11-04 20:04:42 +0530

    Update blog page with dummy data

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,22 @@
+// import Footer from "@/components/Footer";
+import blogs from "@/blogs/data";
+import Link from "next/link";
+
 const Blog = () => {
     return (
-        <div className='h-full pt-32 sm:flex sm:justify-center sm:items-center sm:pt-5'>
-            <h1 className='font-extrabold text-center text-3xl sm:text-6xl'>Coming soon...</h1>
+        <div className='h-full flex justify-center'>
+            <Link href='/' className='max-w-4xl absolute mt-20 px-5 pt-5 pb-10 flex flex-col justify-center items-center gap-[1.85rem] sm:gap-11 sm:pt-10'>
+                {blogs.map((blog, index) => (
+                    <div key={index} className='p-6 border border-[#27272A] rounded-lg flex flex-col justify-center items-start gap-3 sm:p-10 hover:bg-[#040C14]'>
+                        <div>
+                            <h1 className='font-bold text-2xl text-[#ededed]'>{blog.title}</h1>
+                            <h1 className='text-xs mt-[0.425rem] text-[#ededed]/40'>{blog.date}</h1>
+                        </div>
+                        <p className='text-sm leading-[26px] text-[#ededed]/80'>{blog.description}</p>
+                    </div>
+                ))}
+            </Link>
+            {/* <Footer component='blog' /> */}
         </div>
     )
 }
