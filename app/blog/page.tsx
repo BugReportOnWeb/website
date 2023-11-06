ece3167e6235d4b84c2977e479e306fb70db05a2
Dev <flashytesting@gmail.com>
2023-11-07 02:15:38 +0530

    Update prop values of Date component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,5 @@
 // import Footer from "@/components/Footer";
+import Date from "@/components/Date";
 import { getSortedBlogsMetaData } from "@/lib/blogs";
 import Link from "next/link";
 
@@ -13,12 +14,12 @@ const Blogs = () => {
 
     return (
         <div className='h-full flex justify-center'>
-            <div className='max-w-4xl absolute mt-20 px-5 pt-5 pb-10 flex flex-col justify-center items-center gap-[1.85rem] sm:gap-11 sm:pt-10'>
+            <div className='max-w-4xl absolute mt-20 px-5 pt-5 pb-10 flex flex-col justify-center items-center gap-[1.85rem] sm:gap-11 sm:pt-14'>
                 {blogsMetaData.map((metaData, index) => (
                     <Link href={`/blog/${metaData.id}`} key={index} className='w-full p-6 border border-[#27272A] rounded-lg flex flex-col justify-center items-start gap-3 sm:p-10 hover:bg-[#050F19]'>
                         <div>
                             <h1 className='font-bold text-2xl text-[#ededed]'>{metaData.title}</h1>
-                            <h1 className='text-xs mt-[0.425rem] text-[#ededed]/40'>{metaData.date}</h1>
+                            <Date className='text-xs mt-[0.425rem] text-[#ededed]/40' dateString={metaData.date} />
                         </div>
                         <p className='text-sm leading-[26px] text-[#ededed]/80'>{metaData.description}</p>
                     </Link>
