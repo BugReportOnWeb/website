c0073308be1a7c6c85271811b583825dcff579e6
Dev <flashytesting@gmail.com>
2023-11-07 02:17:17 +0530

    Update blog meta data UI with time

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,6 +1,11 @@
-import { getAllBlogIds, getBlogContent, getBlogMetaData } from "@/lib/blogs";
+import Date from "@/components/Date";
+import {
+    getAllBlogIds,
+    getBlogContent,
+    getBlogMetaData
+} from "@/lib/blogs";
 
-interface BlogProp {
+interface BlogProps {
     params: {
         id: string;
     };
@@ -11,23 +16,28 @@ const generateStaticParams = () => {
     return ids
 }
 
-const Blog = async ({ params }: BlogProp) => {
+const Blog = async ({ params }: BlogProps) => {
     const blogMetaData = getBlogMetaData(params.id);
     const blogContent = await getBlogContent(params.id);
 
     return (
         <div className='h-full flex justify-center'>
-            <div className='w-full px-6 pb-32 max-w-4xl absolute mt-24 sm:mt-36'>
+            {/* TODO: Add a back button? */}
+            <div className='w-full px-6 pb-24 max-w-3xl absolute mt-24 sm:mt-36'>
                 <div className='flex flex-col gap-3'>
-                    <h1 className='font-extrabold text-4xl text-[#ededed]'>{blogMetaData.title}</h1>
-                    <h1 className='text-sm text-[#ededed]/60'>{blogMetaData.date}</h1>
+                    <h1 className='font-extrabold text-[2.625rem] text-[#ededed]'>{blogMetaData.title}</h1>
+                    <div>
+                        <p>
+                            <Date className='text-sm text-[#ededed]/60' dateString={blogMetaData.date} />
+                            <span className='text-sm text-[#ededed]/60'> at {blogMetaData.time}</span>
+                        </p>
+                    </div>
                 </div>
                 <div
-                    className='mt-10 parsedown'
+                    className='mt-7 leading-7 parsedown'
                     dangerouslySetInnerHTML={{ __html: blogContent.htmlContent }}
                 />
             </div>
-            {/* TODO: Add a back button? */}
         </div>
     )
 }
