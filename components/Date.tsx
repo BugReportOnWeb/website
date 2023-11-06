b4ac452ceef96f005e6aca5b52b752cfb3cd9acd
Dev <flashytesting@gmail.com>
2023-11-07 02:11:25 +0530

    Add Date component for formating blog date

---
+++ components/Date.tsx
@@ -0,0 +1,18 @@
+import { parseISO, format } from 'date-fns';
+
+interface DateProps {
+    className?: string
+    dateString: string
+}
+
+const Date = ({ className, dateString }: DateProps) => {
+    const date = parseISO(dateString);
+    
+    return (
+        <time className={className} dateTime={dateString}>
+            {format(date, 'dd LLLL yyyy')}
+        </time>
+    )
+}
+
+export default Date;
