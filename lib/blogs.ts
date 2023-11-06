079dcd4f7abd890e7161cd62e8d530337fce277e
Dev <flashytesting@gmail.com>
2023-11-06 19:43:50 +0530

    Add `getAllBlogIds` and `getBlogMetaData` utils

--- lib/blogs.ts
+++ lib/blogs.ts
@@ -6,26 +6,48 @@ import { MetaData } from '@/types/Blog';
 
 const blogsDirectoryPath = path.join(process.cwd(), 'blogs');
 
+// Get all blogs metadata (sorted)
 const getSortedBlogsMetaData = () => {
     const fileNames = fs.readdirSync(blogsDirectoryPath);
 
-    const blogMetaData = fileNames.map(fileName => {
+    const blogsMetaData = fileNames.map(fileName => {
         const id = fileName.replace(/\.md$/, '');
+        const blogMetaData = getBlogMetaData(id);
 
-        const filePath = path.join(blogsDirectoryPath, fileName);
-        const fileContent = fs.readFileSync(filePath);
-
-        // CHECK: Right way to fix optional fields?
-        const matterMetaData = matter(fileContent).data as MetaData;
-        const completeMetaData = { id, ...matterMetaData };
-
-        return completeMetaData;
+        return blogMetaData;
     })
 
-    return blogMetaData.sort((firstBlog, secondBlog) => {
+    return blogsMetaData.sort((firstBlog, secondBlog) => {
         if (firstBlog.date < secondBlog.date) return 1;
         return -1;
     });
 }
 
-export default getSortedBlogsMetaData;
+// Get all blog ids
+const getAllBlogIds = () => {
+    const fileNames = fs.readdirSync(blogsDirectoryPath);
+
+    const blogIds = fileNames.map(fileName => {
+        const id = fileName.replace('/\.md$/', '');
+        return { id };
+    })
+
+    return blogIds;
+}
+
+// Get specific blog metadata
+const getBlogMetaData = (id: string) => {
+    const filePath = path.join(blogsDirectoryPath, `${id}.md`);
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+
+    const matterMetaData = matter(fileContent).data as MetaData;
+    const blogMetaData = { id, ...matterMetaData };
+
+    return blogMetaData;
+}
+
+export { 
+    getSortedBlogsMetaData,
+    getAllBlogIds,
+    getBlogMetaData
+};
