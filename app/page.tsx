fb0a2a24f446b675aa3443e52823525c7a0180d1
Dev <flashytesting@gmail.com>
2023-11-08 01:02:48 +0530

    Change import statement for profile image

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import Footer from "@/components/Footer";
 import Image from "next/image";
 import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+
+import Footer from "@/components/Footer";
+import profileImage from '@/public/profile.png';
 
 const Home = () => {
     return (
         <div className='h-full pt-[6.75rem] sm:flex sm:justify-center sm:items-center sm:pt-0'>
             <div className='relative max-w-sm mx-auto flex flex-col px-5 pb-7 justify-center items-center gap-8 sm:static sm:flex-row-reverse sm:items-start sm:pb-0 sm:max-w-2xl sm:gap-7'>
-                <Image height={170} width={170} className='rounded-full' src='/profile.png' alt="Picture of the author" priority />
+                <Image
+                    height={170}
+                    width={170}
+                    className='rounded-full'
+                    src={profileImage}
+                    placeholder="empty"
+                    alt="Picture of the Dev"
+                />
                 <div>
                     <p className='leading-8'>Hey! I&apos;m Dev, a hobbyist developer hailing from Shimla. Welcome to my corner of the web. I code for fun, creating silly scripts and web apps, and diving into all things tech before 2092. Oh, and music? It&apos;s my true passion and brings me so much joy! Feel free to wander around and check out my work and stuff :)</p>
                     <div className='mt-7 text-xs flex flex-col justify-around gap-3 sm:flex-row'>
