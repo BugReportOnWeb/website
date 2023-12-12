8091a29ba339a6e53000256dc43f192d76dd97fe
Dev <flashytesting@gmail.com>
2023-12-12 19:57:16 +0530

    Update cursor type to copy (discord) and progress (mail)

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -20,7 +20,7 @@ const Contact = () => {
             <div className='mx-auto flex pb-7 px-5 flex-col justify-center items-center w-[26rem] gap-10'>
                 <div className='w-full flex items-start gap-5'>
                     <a href='https://t.me/devkaul' target='_blank' className='text-sm border border-[#242F2B] w-full px-5 py-3 rounded-md flex justify-center items-center cursor-pointer transition-colors ease-in-out bg-transparent hover:bg-[#27272a]'><span><FaTelegramPlane className='mr-2 text-xl' /></span>Telegram</a>
-                    <button onClick={copyUsername} className='relative text-sm border border-[#242F2B] w-full px-5 py-3 rounded-md flex justify-center items-center cursor-pointer transition-colors ease-in-out bg-transparent hover:bg-[#27272a]'>
+                    <button onClick={copyUsername} className='relative text-sm border border-[#242F2B] w-full px-5 py-3 rounded-md flex justify-center items-center cursor-pointer transition-colors ease-in-out bg-transparent hover:bg-[#27272a] hover:cursor-copy'>
                         <span><FaDiscord className='mr-2 text-xl' /></span>Discord
                         <h1 className={`absolute text-[0.85rem] w-full top-0 left-1/2 -translate-x-1/2 -translate-y-[150%] transition-opacity duration-300 ease-in-out ${usernameCopied ? 'opacity-1' : 'opacity-0'}`}>Username copied!</h1>
                     </button>
--- components/ContactFormButton.tsx
+++ components/ContactFormButton.tsx
@@ -7,7 +7,7 @@ const ContactFormButton = () => {
         <button
             type='submit'
             disabled={pending}
-            className='mx-auto bg-[#f8fafc] font-medium text-[#020205] w-full h-10 rounded-md flex justify-center items-center cursor-pointer transition-colors ease-in-out hover:bg-[#f8fafc]/90 disabled:cursor-wait'
+            className='mx-auto bg-[#f8fafc] font-medium text-[#020205] w-full h-10 rounded-md flex justify-center items-center cursor-pointer transition-colors ease-in-out hover:bg-[#f8fafc]/90 disabled:cursor-progress'
         >
             {pending ? 'Sending...' : 'Send Email'}
         </button>
