de6498d9bd825613e6dfecd175a5ef457332edce
Dev <flashytesting@gmail.com>
2024-03-20 21:00:57 +0530

    Add list of projects for home page

---
+++ lib/projects.ts
@@ -0,0 +1,30 @@
+import { Project } from "@/types/project";
+
+const projects: Project[] = [
+    {
+        id: 1,
+        title: 'reponex',
+        description: 'Central hub facilitating open-source collaboration and coordination.',
+        href: 'https://github.com/BugReportOnWeb/reponex'
+    },
+    {
+        id: 2,
+        title: 'roombeat',
+        description: 'A real-time collaborative Spotify music room.',
+        href: 'https://github.com/BugReportOnWeb/roombeat'
+    },
+    {
+        id: 3,
+        title: 'stellar-visions',
+        description: 'Webapp for NASA\'s Astronomy Picture of the Day (APoD).',
+        href: 'https://github.com/BugReportOnWeb/stellar-visions'
+    },
+    {
+        id: 4,
+        title: 'auto-repo-script',
+        description: 'Shell script to automate GitHub repository process.',
+        href: 'https://github.com/BugReportOnWeb/auto-repo-script'
+    },
+]
+
+export default projects;
