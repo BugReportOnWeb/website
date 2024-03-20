8fb080e64d5ec64e2c5802e3b14c07dfebb97bfd
Dev <flashytesting@gmail.com>
2024-03-20 21:05:25 +0530

    Use row alignment with separator on all footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,11 @@
-interface FooterProps {
-    component?: string;
-}
-
-const Footer = ({ component }: FooterProps) => {
-
+const Footer = () => {
     return (
-        <div className={`absolute w-full ${component === 'contact' ? '-bottom-0' : '-bottom-20'} left-0 text-sm px-5 pb-6 flex flex-col gap-1 items-center sm:flex-row sm:justify-between sm:items-end sm:gap-0 sm:bottom-0`}>
-            <h1 className='text-[#e1e7ef]/80 text-xs'>© 2023 Dev. All rights reserved.</h1>
-            <h1 className='text-[#e1e7ef]/80 text-xs'><a className='underline underline-offset-4 decoration-[#e1e7ef]/40 hover:decoration-[#e1e7ef]/80' href='https://github.com/BugReportOnWeb/website' target='_blank'>Source</a> for nerds</h1>
+        <div className='pb-8'>
+            <div className='border border-[#242F2B] mb-8'></div>
+            <div className='flex justify-between items-center text-xs text-[#e1e7ef]/80'>
+                <h1>© 2023 Dev. All rights reserved.</h1>
+                <h1><a className='underline underline-offset-4 decoration-[#e1e7ef]/40 hover:decoration-[#e1e7ef]/80' href='https://github.com/BugReportOnWeb/website' target='_blank'>Source</a> for nerds</h1>
+            </div>
         </div>
     )
 }
