c517710eda0cfa629ae3cf683e1f27eba9a109b9
Dev <flashytesting@gmail.com>
2024-04-20 19:19:20 +0530

    feat: sort blog posts according to their tags

--- components/BlogPageContent.tsx
+++ components/BlogPageContent.tsx
@@ -19,21 +19,41 @@ type BlogPageContentProps = {
 }
 
 const BlogPageContent = ({ blogTags, blogsMetaData }: BlogPageContentProps) => {
-    const [currentTag, setCurrentTag] = useState('#All');
+    const [currentTag, setCurrentTag] = useState('All');
 
     return (
         <div className='max-w-4xl absolute mt-20 px-5 pt-5 pb-10 flex flex-col gap-6'>
             <Tags tags={blogTags} currentTag={currentTag} setCurrentTag={setCurrentTag} />
             <div className="flex flex-col justify-center items-center gap-[1.85rem] sm:gap-6">
-                {blogsMetaData.map(metaData => (
-                    <Link href={`/blog/${metaData.id}`} key={metaData.id} className='w-full p-6 border border-[#27272A] rounded-lg flex flex-col justify-center items-start gap-3 sm:p-9 hover:bg-[#050F19]'>
-                        <div>
-                            <h1 className='font-bold text-2xl text-[#ededed]'>{metaData.title}</h1>
-                            <Date className='text-xs mt-[0.425rem] text-[#ededed]/40' dateString={metaData.date} />
-                        </div>
-                        <p className='text-sm leading-[26px] text-[#ededed]/80'>{metaData.description}</p>
-                    </Link>
-                ))}
+                {currentTag === 'All' ? (
+                    <>
+                        {blogsMetaData.map(metaData => (
+                            <Link href={`/blog/${metaData.id}`} key={metaData.id} className='w-full p-6 border border-[#27272A] rounded-lg flex flex-col justify-center items-start gap-3 sm:p-9 hover:bg-[#050F19]'>
+                                <div>
+                                    <h1 className='font-bold text-2xl text-[#ededed]'>{metaData.title}</h1>
+                                    <Date className='text-xs mt-[0.425rem] text-[#ededed]/40' dateString={metaData.date} />
+                                </div>
+                                <p className='text-sm leading-[26px] text-[#ededed]/80'>{metaData.description}</p>
+                            </Link>
+                        ))}
+                    </>
+                ) : (
+                    <>
+                        {blogsMetaData
+                            .filter(metadata => metadata.tags.includes(currentTag))
+                            .map(metaData => (
+                                <Link href={`/blog/${metaData.id}`} key={metaData.id} className='w-full p-6 border border-[#27272A] rounded-lg flex flex-col justify-center items-start gap-3 sm:p-9 hover:bg-[#050F19]'>
+                                    <div>
+                                        <h1 className='font-bold text-2xl text-[#ededed]'>{metaData.title}</h1>
+                                        <Date className='text-xs mt-[0.425rem] text-[#ededed]/40' dateString={metaData.date} />
+                                    </div>
+                                    <p className='text-sm leading-[26px] text-[#ededed]/80'>{metaData.description}</p>
+                                </Link>
+                            ))
+                        }
+                    </>
+                )}
+
             </div>
         </div>
     )
