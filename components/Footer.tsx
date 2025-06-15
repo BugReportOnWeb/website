360e3d559e35a8a10aede9b63acdffbda1362eb7
Dev <flashytesting@gmail.com>
2025-06-15 13:01:31 +0530

    fix: use current date for footer copyright text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
 const Footer = () => {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+
     return (
         <div className='pb-8'>
             <div className='border border-[#242F2B] mb-8'></div>
             <div className='flex justify-between items-center text-xs text-[#e1e7ef]/80'>
-                <h1>© 2023 Dev. All rights reserved.</h1>
+                <h1>© {currentYear} Dev. All rights reserved.</h1>
                 <h1><a className='underline underline-offset-4 decoration-[#e1e7ef]/40 hover:decoration-[#e1e7ef]/80' href='https://github.com/BugReportOnWeb/website' target='_blank'>Source</a> for nerds</h1>
             </div>
         </div>
